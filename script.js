c21792a364b070f999901485f27675f5236f46e7
Pierre Pierre <pierre.depaz@gmail.com>
2015-06-16 00:23:46 -0400

    dom

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 var stream;
 //var video = document.getElementById('webcam');
 var video = document.querySelector('video');
-
+*/
 document.addEventListener("DOMContentLoaded", function() { 
-
+/*
 	navigator.webkitGetUserMedia(
 		{video: true, audio: true}, // Options
 		function(localMediaStream) { // Success
@@ -15,28 +15,27 @@ document.addEventListener("DOMContentLoaded", function() {
 		function(err) { // Failure
 			alert('getUserMedia failed: Code ' + err.code);
 		}
-	);
+	);*/
+	var video = document.querySelector("video");
+	var constraints = {audio: false, video: true};
+	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||     navigator.mozGetUserMedia;
+
+	function successCallback(stream) 
+	{
+	  window.stream = stream; // stream available to console
+	  if (window.URL) 
+	    {
+	         video.src = window.URL.createObjectURL(stream);
+	    } else 
+	    {
+	         video.src = stream;
+	    }
+	}
+
+	function errorCallback(error)
+	{
+	    console.log("navigator.getUserMedia error: ", error);
+	}
+
+	navigator.getUserMedia(constraints, successCallback, errorCallback);
 });
-*/
-var video = document.querySelector("video");
-var constraints = {audio: false, video: true};
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||     navigator.mozGetUserMedia;
-
-function successCallback(stream) 
-{
-  window.stream = stream; // stream available to console
-  if (window.URL) 
-    {
-         video.src = window.URL.createObjectURL(stream);
-    } else 
-    {
-         video.src = stream;
-    }
-}
-
-function errorCallback(error)
-{
-    console.log("navigator.getUserMedia error: ", error);
-}
-
-navigator.getUserMedia(constraints, successCallback, errorCallback);
