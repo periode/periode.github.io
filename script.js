0e304d18799e110ca42f056b90527ce827bb95b9
Pierre Pierre <pierre.depaz@gmail.com>
2015-06-16 00:17:38 -0400

    script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 var stream;
 //var video = document.getElementById('webcam');
 var video = document.querySelector('video');
@@ -16,3 +17,26 @@ document.addEventListener("DOMContentLoaded", function() {
 		}
 	);
 });
+*/
+var video = document.querySelector("video");
+var constraints = {audio: false, video: true};
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||     navigator.mozGetUserMedia;
+
+function successCallback(stream) 
+{
+  window.stream = stream; // stream available to console
+  if (window.URL) 
+    {
+         video.src = window.URL.createObjectURL(stream);
+    } else 
+    {
+         video.src = stream;
+    }
+}
+
+function errorCallback(error)
+{
+    console.log("navigator.getUserMedia error: ", error);
+}
+
+navigator.getUserMedia(constraints, successCallback, errorCallback);
