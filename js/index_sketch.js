8ce1d6e23c4467e493feab80bd2508290dacf7f4
Pierre Depaz <pierre.depaz@gmail.com>
2016-03-02 01:02:46 -0500

    artist animations + other pages

--- js/index_sketch.js
+++ js/index_sketch.js
@@ -1,93 +1,233 @@
-var pos = [];
-var pos2 = [];
-var progress = 0;
-var progress2;
+var pos_gabor = [];
+var pos_maro = [];
+var pos_myles = [];
+
+var lerp_inc = 0.1;
+var lerp_gabor = 0;
+
+
+var progress_gabor = 0;
+var progress_maro = 0;
+var progress_myles = 0;
+
 var step;
 var drawArtist = false;
+var addingVector = false;
 
 var artist;
 var gabor;
 var otodojo;
+var myles;
+
+var periode = ['p','e','r','i','o','d','e'];
+
+var fg_col = 0;
+var bg_col = 255;
+
+var interval_vector = 100;
+
+var middle_line_top;
 
 function setup(){
   var cnv = createCanvas(windowWidth, windowHeight);
   cnv.position(0, 0);
-  progress2 = width;
 
   step = width*0.025;
+  middle_line_top = height;
+  middle_line_top = height*0.3;
 
-  pos[0] = createVector(progress, height*0.7);
-  pos2[0] = createVector(progress2, height*0.7);
-  background(0);
-
-  setInterval(addVector, 50);
+  pos_gabor[0] = createVector(progress_gabor, height*0.6);
+  pos_maro[0] = createVector(progress_maro, height*0.6);
+  pos_myles[0] = createVector(progress_maro, height*0.6);
+  background(255);
 
   var title = addElement('title');
-  title.innerHTML = 'define';
+  title.innerHTML = 'MENTION';
   title.setAttribute('href', 'index.html');
+  title.style.top = '2%';
+
+  var audio = addElement('audio');
+  audio.innerHTML = 'audio';
+  audio.setAttribute('href', 'audio.html');
+
+  var audio = addElement('video');
+  video.innerHTML = 'video';
+  video.setAttribute('href', 'video.html');
+
+  var info = addElement('info');
+  info.innerHTML = 'info';
+  info.setAttribute('href', 'info.html');
 }
 
 function draw(){
-  background(0);
+  background(bg_col);
 
-  for(var i = 0; i < pos.length-1; i++){
-    stroke(255);
-    line(pos[i].x, pos[i].y, pos[i+1].x, pos[i+1].y);
+  drawFirstLine();
+  for(var i = 0; i < pos_gabor.length-1; i++){
+    stroke(fg_col);
+    strokeWeight(2);
+    if(i < pos_gabor.length-2){
+      line(pos_gabor[i].x, pos_gabor[i].y, pos_gabor[i+1].x, pos_gabor[i+1].y);
+    }else{
+      var pos_temp = p5.Vector.lerp(pos_gabor[i], pos_gabor[i+1], lerp_gabor);
+      line(pos_gabor[i].x, pos_gabor[i].y, pos_temp.x, pos_temp.y);
+    }
 
     for(var j = 0; j < 5; j++){
-      stroke(255, (mouseY-j*50-height*0.5));
-      fill(255, mouseY-j*100);
-      line(pos[i].x, pos[i].y+j*10, pos[i+1].x, pos[i+1].y+j*10);
+      strokeWeight(1);
+      stroke(fg_col, (mouseY-j*50-height*0.3));
+      fill(fg_col, mouseY-j*100);
+      line(pos_gabor[i].x, pos_gabor[i].y-j*05*constrain(sin(millis()*0.001+i*0.1), -10, 10), pos_gabor[i+1].x, pos_gabor[i+1].y-j*10*cos(millis()*0.001+i*0.1+1));
     }
   }
 
-  for(var i = 0; i < pos2.length-1; i++){
-    stroke(255);
-    line(pos2[i].x, pos2[i].y, pos2[i+1].x, pos2[i+1].y);
+  for(var i = 0; i < pos_maro.length-1; i++){
+    stroke(fg_col);
+    strokeWeight(2);
+    if(i < pos_maro.length-2){
+      line(pos_maro[i].x, pos_maro[i].y, pos_maro[i+1].x, pos_maro[i+1].y);
+    }else{
+      var pos_temp = p5.Vector.lerp(pos_maro[i], pos_maro[i+1], lerp_gabor);
+      line(pos_maro[i].x, pos_maro[i].y, pos_temp.x, pos_temp.y);
+    }
 
     for(var j = 0; j < 5; j++){
-      stroke(255, (mouseY-j*50-height*0.5));
-      fill(255, mouseY-j*100);
-      line(pos2[i].x, pos2[i].y-j*10*cos(millis()*0.001+i*0.1), pos2[i+1].x, pos2[i+1].y-j*10*cos(millis()*0.001+i*0.1));
+      strokeWeight(1);
+      stroke(fg_col, (mouseY-j*50-height*0.3));
+      fill(fg_col, mouseY-j*100);
+      line(pos_maro[i].x, pos_maro[i].y-j*05*min(sin(millis()*0.001+i*0.1+1), 10), pos_maro[i+1].x, pos_maro[i+1].y-j*10*cos(millis()*0.001+i*0.1));
     }
   }
 
-  if(drawArtist && gabor == null){
-    console.log(gabor);
-    gabor = addElement('gabor');
-    gabor.innerHTML = 'gabor';
-    gabor.setAttribute('href', 'https://soundcloud.com/mesmarecords');
-    gabor.style.top = pos[i].y;
-    gabor.style.left = pos[i].x+10;
-    gabor.style.color = 'white';
-    gabor.style.fontFamily = 'Trebuchet MS';
+  for(var i = 0; i < pos_myles.length-1; i++){
+    stroke(fg_col);
+    strokeWeight(2);
+    if(i < pos_myles.length-2){
+      line(pos_myles[i].x, pos_myles[i].y, pos_myles[i+1].x, pos_myles[i+1].y);
+    }else{
+      var pos_temp = p5.Vector.lerp(pos_myles[i], pos_myles[i+1], lerp_gabor);
+      line(pos_myles[i].x, pos_myles[i].y, pos_temp.x, pos_temp.y);
+    }
+
+
+    for(var j = 0; j < 5; j++){
+      strokeWeight(1);
+      stroke(fg_col, (mouseY-j*50-height*0.3));
+      fill(fg_col, mouseY-j*100);
+      line(pos_myles[i].x, pos_myles[i].y-j*05*min(sin(millis()*0.001+i*0.1+2), 10), pos_myles[i+1].x, pos_myles[i+1].y-j*4*cos(millis()*0.001+i*0.1));
+    }
+  }
+drawMiddleLine();
+
+
+  if(drawArtist){
+
+    drawPeriode();
+    if(gabor == null){
+      gabor = addElement('gabor');
+      gabor.innerHTML = 'gabor';
+      gabor.setAttribute('href', 'https://soundcloud.com/mesmarecords');
+      gabor.setAttribute('id', 'artist');
+      gabor.style.top = pos_gabor[i].y;
+      gabor.style.left = pos_gabor[i].x+15;
+    }
+
+    if(otodojo == null){
+      otodojo = addElement('otodojo');
+      otodojo.innerHTML = 'otodojo';
+      otodojo.setAttribute('href', 'https://soundcloud.com/otodojo');
+      otodojo.setAttribute('id', 'artist');
+      otodojo.style.top = pos_maro[i].y;
+      otodojo.style.left = pos_maro[i].x+15;
+      otodojo.style.textAlign = 'right';
+    }
+
+    if(myles == null){
+      myles = addElement('myles');
+      myles.innerHTML = 'myles avery';
+      myles.setAttribute('href', 'https://soundcloud.com/mylesavery');
+      myles.setAttribute('id', 'artist');
+      myles.style.top = pos_myles[i].y;
+      myles.style.left = pos_myles[i].x+15;
+      myles.style.textAlign = 'right';
+    }
   }
 
-  if(drawArtist && otodojo == null){
-    otodojo = addElement('otodojo');
-    otodojo.innerHTML = 'otodojo';
-    otodojo.setAttribute('href', 'https://soundcloud.com/otodojo');
-    otodojo.style.top = pos2[i].y;
-    otodojo.style.left = pos2[i].x-10;
-    otodojo.style.textAlign = 'right';
-    otodojo.style.color = 'white';
-    otodojo.style.fontFamily = 'Trebuchet MS';
+  if(addingVector){
+    if(lerp_gabor < 1)
+      lerp_gabor += lerp_inc;
+    else
+      addVector();
   }
+
 
   noCursor();
-  stroke(255);
+  noFill();
+  stroke(0);
   ellipse(mouseX, mouseY, 10, 10);
 }
 
-function addVector(){
-  if(progress < width*0.45){
-    progress += step;
-    var v = createVector(progress, height*0.7-noise(millis()*0.0025)*height*0.25);
+function drawFirstLine(){
+  strokeWeight(2);
+  var h = parseInt(document.defaultView.getComputedStyle(title, "").getPropertyValue("height").substring(0, 2))+2;
 
-    progress2 -= step;
-    var v2 = createVector(progress2, height*0.7-noise(millis()*0.0025+100)*height*0.25);
-    pos2.push(v2);
-    pos.push(v);
+  var end = createVector(max(width*0.45, width*0.45+tan(millis()*0.001)*width*0.1), h);
+  line(width*0.45, h, end.x, end.y);
+
+  if(end.x > width && !addingVector){
+    // setInterval(addVector, interval_vector);
+    addingVector = true;
+  }
+}
+
+function drawMiddleLine(){
+  strokeWeight(1);
+  stroke(0);
+  for(var i = -1; i < 2; i++){
+    line(width*0.5+i*5, height, width*0.5+i*2, middle_line_top);
+  }
+  textSize(24);
+  textAlign(RIGHT);
+  text('sound ', width*0.5, middle_line_top);
+  textAlign(LEFT);
+  text('  visuals', width*0.5, middle_line_top);
+  textAlign(CENTER);
+  text('  electronic', width*0.5, middle_line_top*0.85);
+
+  if(middle_line_top > height*0.3)
+    middle_line_top-= 20;
+
+}
+
+function drawPeriode(){
+  fill(0);
+  noStroke();
+  textSize(16);
+  for(var i = 0; i < periode.length; i++){
+    fill(0);
+    noStroke();
+    text(periode[i], width*0.75+i*10, height*0.5+cos(millis()*0.001+map(i, 0, periode.length, 0, PI))*height*0.2);
+    stroke(fg_col, (mouseY-i*30-height*0.3));
+    line(width*0.75+i*10, height, width*0.75+i*10, height*0.55+cos(millis()*0.001+map(i, 0, periode.length, 0, PI))*height*0.2)
+  }
+}
+
+function addVector(){
+  if(progress_gabor < width*0.3){
+    progress_gabor += step;
+    var v = createVector(progress_gabor, height*0.55-noise(millis()*0.0005)*height*0.25);
+    lerp_gabor = 0;
+
+    progress_maro += step;
+    var v2 = createVector(progress_maro, height*0.65-noise(millis()*0.0005+100)*height*0.25);
+
+    progress_myles +=  step;
+    var v3 = createVector(progress_myles, height*0.75-noise(millis()*0.0005+200)*height*0.25);
+
+
+    pos_gabor.push(v);
+    pos_maro.push(v2);
+    pos_myles.push(v3);
   }else{
     drawArtist = true;
   }
@@ -98,6 +238,5 @@ function addElement(str){
   e.setAttribute('id', str);
   e.style.position = 'absolute';
   document.body.appendChild(e);
-  console.log(e);
   return e;
 }
---
+++ js/video_sketch.js
@@ -0,0 +1,12 @@
+
+
+function setup(){
+  var cnv = createCanvas(windowWidth, windowHeight);
+  cnv.position(0, 0);
+
+
+}
+
+function draw(){
+
+}
