399481e93a8312357d1c7c5bff0e864de4d4e8d4
Pierre Depaz <pierre.depaz@gmail.com>
2016-03-28 11:29:00 -0400

    alpha links

--- over-saturation/link.js
+++ over-saturation/link.js
@@ -3,13 +3,13 @@ var Link = function(_pos1, _pos2, _col, _i){
   this.pos2 = _pos2.copy();
   this.col = color(red(_col), blue(_col), green(_col));
   this.index = _i;
-  this.alpha = 10;
+  this.alpha = 50;
 
   this.display = function(){
     stroke(red(this.col), blue(this.col), green(this.col), this.alpha);
     line(this.pos1.x, this.pos1.y, this.pos2.x, this.pos2.y);
 
-    this.alpha -= 0.0075;
+    this.alpha -= 0.075;
     if(this.alpha < 0)
       links.splice(this.index, 1);
   }
