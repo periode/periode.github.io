5a9681f5464a9e4d1a7d05db7b75f9805f33d1c7
Pierre Depaz <pierre.depaz@gmail.com>
2017-05-29 20:50:31 -0400

    first pass getting data from sheets

---
+++ cds/data-grab.js
@@ -0,0 +1,48 @@
+//using this to clean data: http://gsx2json.com/
+var SPREADSHEET_URL = "http://gsx2json.com/api?id=1iqTPWhv5OdzZAR9qzRidtDgpz3u4BGHrykTvtN4QcRo";
+
+
+function init(){
+    $.getJSON(SPREADSHEET_URL, function(data){
+	    console.log(data);
+	    for(var i = 0; i < data.rows.length; i++){
+		addPosting(data.rows[i]);
+	    }
+    });
+}
+
+
+function addPosting(item){
+    var e = document.createElement('div');
+	e.setAttribute('class', 'posting');
+
+	var title = document.createElement('span');
+	title.setAttribute('class', 'title');
+	title.innerText = (item.title == null) ? 'PROJECT' : item.title;
+	e.append(title);
+
+	var name = document.createElement('span');
+	name.setAttribute('class', 'name');
+	name.innerHTML = item.name;
+	e.append(name);
+
+	var department = document.createElement('span');
+	department.setAttribute('class', 'department');
+	department.innerHTML = item.department;
+	e.append(department);
+	
+	var description = document.createElement('span');
+	description.setAttribute('class', 'description');
+	description.innerHTML = item.description;
+	e.append(description);
+	
+	var needs = document.createElement('span');
+	needs.setAttribute('class', 'needs');
+	needs.innerHTML = item.needs;
+	e.append(needs);
+
+	var line = document.createElement('hr');
+	e.append(line);
+
+	document.getElementById('container').append(e);
+}
